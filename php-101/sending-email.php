59219cfaa6de53518efb1cdb5168476a150827c8
kevin.m.guertin <kevin.m.guertin@gmail.com>
2020-05-18 12:32:20 -0400

    send email via php

---
+++ php-101/sending-email.php
@@ -0,0 +1,20 @@
+<?php
+  //Recipient
+$to = 'kevin.m.guertin@gmail.com';
+
+//Subject
+$subject = 'This is a test email!';
+
+//Message 
+$message = '<h1>Hi There.</h1><p>Thanks for testing!</p>';
+
+//Header
+$headers = "From: The Sender Name <phpemailtest@test.com>\r\n";
+$headers .="Reply-To: replyto@phptest.com\r\n"; 
+$headers .= "Content-type: text/html\r\n";
+
+//Send Email
+mail($to, $subject, $message, $headers);
+
+
+?>
